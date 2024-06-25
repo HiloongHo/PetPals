1f01dcf8543b5877cb209195e3ea605851db896f
nathanhe <nathanhe@88vip.site>
2024-06-25 14:03:18 +0800

    finished project

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,6 @@ class MyApp extends StatelessWidget { // MyApp类，继承自StatelessWidget
       theme: ThemeData(                   // 设置主题数据
         useMaterial3: true,               // 使用Material3主题
       ),
-
-
       home: const StartScreen1(),              // 设置应用程序的初始页面为StartScreen1
     );
   }
